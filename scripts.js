9156fbfc8e0ddf76ea5a04bffa81bc71a1a3d9d9
LailaL88 <58104146+LailaL88@users.noreply.github.com>
2019-12-21 13:52:33 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,77 @@
+
+
+function onPageLoaded() {
+    const input = document.querySelector("input[type='text']");
+    const ul = document.querySelector("ul");
+    const saveButton = document.querySelector("button.save");
+
+    function createTodo() {
+        
+        const li = document.createElement("li");
+        const textSpan = document.createElement("span");
+        
+        const newTodo = input.value;
+        textSpan.append(newTodo);
+
+   
+        const deleteBtn = document.createElement("span");
+        deleteBtn.classList.add("todo-trash");
+        const icon = document.createElement("i");
+        icon.classList.add("fas", "fa-eraser");
+        deleteBtn.appendChild(icon);
+
+        ul.appendChild(li).append(textSpan, deleteBtn);
+        input.value = "";
+        deleteButton(deleteBtn);
+    }
+
+    saveButton.addEventListener("click", () => {
+            localStorage.setItem("todos", ul.innerHTML);
+       });
+
+     function loadTodos() {
+        const data = localStorage.getItem("todos");
+        if (data) {
+            ul.innerHTML = data;
+           }
+        const deleteButtons = document.querySelectorAll("span.todo-trash");
+            for (const button of deleteButtons) {
+                deleteButton(button);
+            }
+        }
+
+    loadTodos();
+
+     function deleteButton(element) {
+        element.addEventListener("click", (event) => {
+            element.parentElement.remove();
+            event.stopPropagation();
+        });
+    }
+
+    input.addEventListener("keypress", (keyPressed) => {
+        const keyEnter = 13;
+        if (keyPressed.which == keyEnter) {
+            createTodo();
+        }
+    });
+}
+
+document.addEventListener("DOMContentLoaded", onPageLoaded);
+
+function showTips(){
+        document.getElementById("tip").style.display = "block";
+    }
+
+function hideTips(){
+    document.getElementById("tip").style.display = "none";
+}
+
+function clearList(){
+    var clear = document.getElementById("list");
+    clear.innerHTML="";
+}
+
+
+
+
